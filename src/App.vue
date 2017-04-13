9f4cd9bbd893a1478fff0dd7063ad6552833f972
jharmon141 <jeremy.l.harmon@gmail.com>
2017-04-12 21:26:08 -0500

    :robot:

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="app">
 
-        <router-view></router-view>
+        <router-view :user="user"></router-view>
 
     </div>
 
@@ -11,11 +11,12 @@
 import ListPage from './components/ListPage.vue'
 import NewPostLink from './components/NewPostLink.vue'
 import LoginAuth0 from './components/LoginAuth0.vue'
+import Auth0Lock from 'auth0-lock'
 import gql from 'graphql-tag'
 
 const userQuery = gql`
-  query userQuery {
-    user {
+  query($id: String!) {
+    user(id: $id) {
       id
     }
   }
@@ -25,7 +26,8 @@ export default {
     name: 'app',
     data: () => ({
         authenticated: false,
-        user: {}
+        user: {},
+        lock: new Auth0Lock('AJhg1KjBM1iEAof3307nVM34qnJwlqqD', 'jharmon141.auth0.com'),
     }),
     components: {
         'login': LoginAuth0,
@@ -41,10 +43,31 @@ export default {
         logout() {
             // To log out, we just need to remove the token and profile
             // from local storage
-            localStorage.removeItem('id_token');
-            localStorage.removeItem('profile');
-            this.authenticated = false;
+            localStorage.removeItem('id_token')
+            localStorage.removeItem('profile')
+            this.authenticated = false
         },
+    },
+
+    mounted() {
+
+        this.lock.on('authenticated', (authResult) => {
+            console.log('authenticated')
+            localStorage.setItem('id_token', authResult.idToken)
+            this.lock.getProfile(authResult.idToken, (error, profile) => {
+                if (error) {
+                    // Handle error
+                    return
+                }
+                // Set the token and user profile in local storage
+                localStorage.setItem('profile', JSON.stringify(profile))
+
+                this.authenticated = true
+            })
+        })
+        this.lock.on('authorizaton_error', (error) => {
+            // handle error when authorizaton fails
+        })
     },
 
     apollo: {
@@ -55,41 +78,40 @@ export default {
             },
         },
     }
-
-
 }
+
 </script>
 
 <style>
 
 body, input, button {
-  font-family: Helvetica, sans-serif;
-  font-size: 16pt;
+    font-family: Helvetica, sans-serif;
+    font-size: 16pt;
 }
 
 ul li {
-  list-style: none;
+    list-style: none;
 }
 
 .app {
-  text-align: center;
-  display: flex;
-  justify-content: center;
+    text-align: center;
+    display: flex;
+    justify-content: center;
 
-  padding: 12px;
-  margin: auto;
+    padding: 12px;
+    margin: auto;
 }
 
 input:focus {
-  box-shadow: none;
-  outline: none;
-  border-color: #40b883;
+    box-shadow: none;
+    outline: none;
+    border-color: #40b883;
 }
 
 input {
-  padding: 8px;
-  border: solid 1px #bbb;
-  border-radius: 2px;
+    padding: 8px;
+    border: solid 1px #bbb;
+    border-radius: 2px;
 }
 
 
--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -33,28 +33,34 @@ const userQuery = gql`
 
 export default {
 
-  data: () => ({
-    email: '',
-    name: '',
-    emailSubscription: false
-  }),
+    props: {
+        user: {},
+    },
+
+    data: () => ({
+        email: '',
+        name: '',
+        emailSubscription: false
+    }),
+
+    mounted() {
+
+        // redirect if user is logged in or did not finish Auth0 Lock dialog
+        if (this.user || window.localStorage.getItem('auth0IdToken') === null) {
+            console.warn('not a new user or already logged in')
+            this.$router.push({ name: 'Home' });
+        }
+
+    },
 
     methods: {
 
-        toggleSubscription() {
-            if (!this.emailSubscription) {
-                this.emailSubscription = true
-            } else {
-                this.emailSubscription = false
-            }
-        },
-
         create() {
 
-                let idToken = localStorage.getItem('auth0IdToken')
-                let emailAddress = this.email
-                let name = this.name
-                let emailSubscription = this.emailSubscription
+            let idToken = localStorage.getItem('auth0IdToken')
+            let emailAddress = this.email
+            let name = this.name
+            let emailSubscription = this.emailSubscription
 
             // Mutation
             this.$apollo.mutate({
@@ -65,9 +71,18 @@ export default {
                     name,
                     emailSubscription,
                 },
+                updateQueries: {
+                    user: (prev, { mutationResult }) => {
+                        return {
+                            // append at head of list because we sort the posts reverse chronological
+                            user: [mutationResult.data.createUser, ...prev.user],
+                        }
+                    },
+                },
             }).then((data) => {
                 // Result
                 console.log(data)
+                this.$router.push({ name: 'Home' });
             }).catch((error) => {
                 // Error
                 console.error(error)
@@ -80,9 +95,9 @@ export default {
 <style>
 
 .createUser {
-  text-align: center;
-  display: flex;
-  justify-content: flex-start;
-  flex-direction: column;
+    text-align: center;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
 }
 </style>
